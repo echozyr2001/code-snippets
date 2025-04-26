dfb7338d3df7be40fdeeeb24848ef1c91c7ef08f
echozyr2001 <echo.zyr.2001@gmail.com>
2025-04-26 17:47:41 +0800

    Github button

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import LangSelect from "@/components/controlers/LanguageSelect";
 import ThemeSelect from "@/components/controlers/ThemeSelect";
 import { ExportButton } from "@/components/controlers/ExportButton";
 import { Card, CardContent } from "@/components/ui/card";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="dark h-screen flex flex-col gap-4 justify-center items-center bg-neutral-950 text-white p-4">
+      <Header />
       <CodeEditor id="code-editor" />
       <Card className="p-6 w-fit bg-neutral-900/90 backdrop-blur">
         <CardContent className="flex flex-wrap gap-4 sm:gap-6 p-0">
